ee4df97d6dfdc03eaa3750a4f3c900f01bbe2884
Peter Svec <peter,svec7@gmail.com>
2024-12-16 19:05:11 +0100

    Hadik ukoncena hra

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>fun times php</title>
+
+</head>
+<body class="container">
+
+	<div class="page-header">
+		<h1 class="text-muted">fun times php</h1>
+	</div>
+
+    <?php
+    $item = 5;
+    echo $item;
+    echo'<br>';
+    echo'<br>';
+    echo $item/2;
+    ?>
+</body>
+</html>
